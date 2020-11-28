b291e4d3a13d1029e1db97789fe7ad13c66c336a
jagtapa <arunjagtap38@gmail.com>
2020-11-28 21:32:57 +0530

    aws learning day 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! I am coming 4EVER'));
+app.get('/', (req, res) => res.send('Hello World! I am coming 4EVER DAY2'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
